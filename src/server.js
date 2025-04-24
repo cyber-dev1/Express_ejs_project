2db97bb78da7a4bd615551352862745b70721426
cyber-dev1 <ogabek200825@gmail.com>
2025-04-24 16:19:33 +0500

    Added-errPage-and-Layot-page

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const layout = require("express-ejs-layouts")
 const { mainRoutes } = require("./routes/main.routes");
 const { viewsRouter } = require("./routes/views.routes");
 const { config } = require('dotenv');
@@ -10,11 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(process.cwd(), "public")));
+app.use(layout);
+app.set("layout", "layout/main")
 app.set("view engine", "ejs") ;
 app.set("views", path.join(process.cwd(), "src", "views"))
 
 app.use("/api", mainRoutes);
 app.use(viewsRouter);
+app.use((req, res) => {
+    let url = req.url.trim().toLowerCase() ;
+    let method = req.method.trim().toUpperCase() ;
+
+    return res.render("errPage", {url, method, page : "Error page"})
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server is running on port ${process.env.PORT}`)
