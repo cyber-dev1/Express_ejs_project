fac38a59ad4ebc5418b5ef9e24e6a0ec0817a70a
cyber-dev1 <ogabek200825@gmail.com>
2025-04-24 16:52:27 +0500

    fixed bug

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,7 +1,7 @@
 const path = require("path")
 class viewsController {
     constructor(){
-        this.MAIN = (req, res) => res.render("index", {page : "Bosh sahifa"}) ;
+        this.MAIN = (req, res) => res.render("index", {page : "Bosh sahifa", gUrl : "/css/index.css"}) ;
         this.LOGIN = (req, res) => res.render("login", {page : "Login sahifasi"}) ;
         this.REGISTER = (req, res) => res.render("register", {page : "Register sahifasi"}) ;
     }
